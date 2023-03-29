d813f5677caf79b418076cfc55b0ec2c2618871e
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2023-03-29 14:29:44 -0400

    Added an employment page to the navigation and retrieved and listed employment from the database

--- plugins/cms.js
+++ plugins/cms.js
@@ -139,6 +139,30 @@ const CMS_PLUGIN = {
 			},
 			{
 				method: "GET",
+				path: "/employment",
+				options: {
+					auth: 'customAuth'
+				},
+				handler: async function(req,h) {
+					try {
+						let client = await mongoClient.connect();
+						let db = client.db('portfolio-cms');
+
+						let employment = await db.collection('employment').find({}).toArray();
+
+						return h.view('employment', { employment });
+					} catch(err) {
+						let { user } = req.auth.credentials
+
+						console.log("Error occured");
+						console.log(err);
+
+						return h.view('index', { name: user });
+					}
+				}
+			},
+			{
+				method: "GET",
 				path: "/contact",
 				options: {
 					auth: 'customAuth'
