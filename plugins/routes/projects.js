2ec4b1cd979579bc7c10abb86186ea7a3036938e
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2024-04-26 12:45:30 -0400

    Added functionality for adding new project and handling error and success messages.

--- plugins/routes/projects.js
+++ plugins/routes/projects.js
@@ -11,13 +11,16 @@ const routes = [
 			auth: 'customAuth'
 		},
 		handler: async function(req,h) {
+			let message = req.query['msg'];
+			let error = req.query['err'];
+
 			try {
 				let client = await mongoClient.connect();
 				let db = client.db('portfolio_cms');
 
 				let projects = await db.collection('projects').find({}).toArray();
-
-				return h.view('projects/list', { projects });
+				
+				return h.view('projects/list', { projects, message, error });
 			} catch(err) {
 				console.log("Error occured");
 				console.log(err);
@@ -32,6 +35,45 @@ const routes = [
 		},
 		handler: function(req,h) {
 			return h.view('projects/add');
+		}
+	},
+	{
+		method: 'POST',
+		path: '/projects/create',
+		options: {
+			auth: 'customAuth'
+		},
+		handler: async function(req,h) {
+			let client;
+			try {
+				client = await mongoClient.connect();
+				let db = client.db('portfolio_cms');
+
+				let submittedData = req['payload'];
+				let newProject = {
+					title: submittedData['title'],
+					description: submittedData['description'],
+					github_url: submittedData['github_url'],
+					live_url: submittedData['live_url']
+				}
+
+				let result = await db.collection('projects').insertOne(newProject);
+
+				let successParam = new URLSearchParams({
+					success: "Successfully created new project"
+				})
+				return h.redirect(`/cms/projects?${successParam.toString()}`);
+			} catch(err) {
+				console.log("Error creating project");
+				console.log(err);
+
+				let errParam = new URLSearchParams({
+					err: "Error creating project"
+				});
+				return h.redirect(`/cms/projects?${errParam.toString()}`);
+			} finally {
+				client.close();
+			}
 		}
 	},
 	{
