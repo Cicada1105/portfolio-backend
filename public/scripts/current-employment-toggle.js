779364bd4fa2699732c94eed3d8fab97bfa575f5
Joshua Colvin <joshicolvin@gmail.com>
2024-09-06 19:23:01 -0400

    Added start/end date verification for adding a new employment entry.

--- public/scripts/current-employment-toggle.js
+++ public/scripts/current-employment-toggle.js
@@ -1,23 +1,25 @@
-let isCurrentEmploymentEl = document.getElementById('is-current-employment');
+(function() {
+  const isCurrentEmploymentEl = document.getElementById('is-current-employment');
 
-let endMonthInput = document.getElementById('end-month');
-let endMonthInputCtrl = endMonthInput.parentElement;
-let endMonthLabel = endMonthInputCtrl.getElementsByTagName('label')[0];
+  const endMonthInput = document.getElementById('end-month');
+  const endMonthInputCtrl = endMonthInput.parentElement;
+  const endMonthLabel = endMonthInputCtrl.getElementsByTagName('label')[0];
 
-let endYearInput = document.getElementById('end-year');
-let endYearInputCtrl = endYearInput.parentElement;
-let endYearLabel = endYearInputCtrl.getElementsByTagName('label')[0]
+  const endYearInput = document.getElementById('end-year');
+  const endYearInputCtrl = endYearInput.parentElement;
+  const endYearLabel = endYearInputCtrl.getElementsByTagName('label')[0]
 
-isCurrentEmploymentEl.addEventListener('change',() => {
-  let isCurrent = isCurrentEmploymentEl.checked;
-  let currentColor = isCurrent ? 'light-dark(graytext, rgb(170, 170, 170))' : 'black'; 
-  let currentBorderColor = isCurrent ? 'rgba(118, 118, 118, 0.3)' : 'black';
-  let show = isCurrent ? 'none' : 'flex';
-  
-  endMonthLabel.style.color = currentColor;
-  endMonthInput.toggleAttribute('disabled');
-  endMonthInput.style.borderColor = currentBorderColor;
-  endYearLabel.style.color = currentColor;
-  endYearInput.toggleAttribute('disabled');
-  endYearInput.style.borderColor = currentBorderColor;
-})
+  isCurrentEmploymentEl.addEventListener('change',() => {
+    let isCurrent = isCurrentEmploymentEl.checked;
+    let currentColor = isCurrent ? 'light-dark(graytext, rgb(170, 170, 170))' : 'black'; 
+    let currentBorderColor = isCurrent ? 'rgba(118, 118, 118, 0.3)' : 'black';
+    let show = isCurrent ? 'none' : 'flex';
+    
+    endMonthLabel.style.color = currentColor;
+    endMonthInput.toggleAttribute('disabled');
+    endMonthInput.style.borderColor = currentBorderColor;
+    endYearLabel.style.color = currentColor;
+    endYearInput.toggleAttribute('disabled');
+    endYearInput.style.borderColor = currentBorderColor;
+  })
+})()
