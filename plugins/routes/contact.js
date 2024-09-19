b8bec14036c7647b1e3cbeefe471ab4cbc6422ba
Joshua Colvin <joshicolvin@gmail.com>
2024-09-19 16:02:20 -0400

    Installed ImageKit SDK for NodeJS image uploads
    Can now upload image when adding a new contact platform.

--- plugins/routes/contact.js
+++ plugins/routes/contact.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const crypto = require('crypto');
+const ImageKit = require('imagekit');
 // Local 
 const { mongoClient, ObjectId } = require('../../utils/mongodb.js');
 
@@ -48,11 +50,79 @@ const routes = [
 		method: 'POST',
 		path: '/contact/create',
 		options: {
-			auth: 'customAuth'
+			auth: 'customAuth',
+			payload: {
+        output: 'stream',
+        parse: true,
+        allow: 'multipart/form-data',
+        multipart: true
+      }
 		},
-		handler: function(req, h) {
-			console.log(req.payload);
-			return h.redirect('/cms/contact');
+		handler: async function(req, h) {
+			let params;
+			try {
+				const client = await mongoClient.connect();
+				const db = client.db(DB_NAME);
+				
+				let { platform, handle, url, icon: { _data, hapi: { filename } } } = req.payload;
+
+				let fileExtension = filename.split('.')[1];
+
+				let contactInfo = {
+					platform: {
+						name: platform
+					},
+					handle,
+					url
+				}
+
+				var imageKit = new ImageKit({
+				    publicKey : process.env.IMAGE_KIT_PUBLIC_KEY,
+				    privateKey : process.env.IMAGE_KIT_PRIVATE_KEY,
+				    urlEndpoint : `https://ik.imagekit.io/${process.env.IMAGE_KIT_ID}/`
+				});
+
+				const IMAGE_UPLOAD_OPTIONS = {
+					file: _data.toString('base64'),
+					fileName: `${crypto.randomUUID()}.${fileExtension}`
+				}
+
+				let callbackFunc = async function(err, result) {
+			    if(err) {
+			    	console.log("Error uplaoding to ImageKit");
+			    	console.log(err);
+
+						params = new URLSearchParams({
+							err: "Error creating contact record"
+						})
+			    }
+			    else {
+			    	let { fileId, url, thumbnailUrl } = result;
+			    	contactInfo['platform'].icon = {
+			    		id: fileId,
+			    		url,
+			    		thumbnailUrl
+			    	}
+
+				    let dbResult = await db.collection(COLLECTION_NAME).insertOne(contactInfo);
+
+						params = new URLSearchParams({
+							success: "Successfully created new contact record"
+						});
+			    }
+				}
+
+				imageKit.upload(IMAGE_UPLOAD_OPTIONS, callbackFunc);
+			} catch(err) {
+				console.log(`Error creating new contact: ${req['params'].name}`);
+				console.log(err);
+
+				params = new URLSearchParams({
+					err: "Error creating contact record"
+				})
+			} finally {
+				return h.redirect(`/cms/contact?${params.toString()}`);
+			}
 		}
 	},
 	{
