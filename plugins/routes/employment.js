eeb147e9a69ea26ec9cc235954572e0270f4580f
Joshua Colvin <joshicolvin@gmail.com>
2024-09-25 20:06:51 -0400

    Added ability to update employment records.

--- plugins/routes/employment.js
+++ plugins/routes/employment.js
@@ -86,10 +86,86 @@ const routes = [
 		options: {
 			auth: 'customAuth'
 		},
-		handler: function(req,h) {
+		handler: async function(req,h) {
 			let { id } = req.params;
 			
-			return h.view('employment/edit', { id });
+			try {
+				const client = await mongoClient.connect();
+				const db = client.db(DB_NAME);
+
+				let result = await db.collection(COLLECTION_NAME).findOne({
+					_id: new ObjectId(id)
+				})
+
+				return h.view('employment/edit', result);
+			} catch(err) {
+				console.log(`Error retrieving employment data to edit with id: ${id}`);
+				console.log(err);
+
+				let params = new URLSearchParams({
+					err: "Error retrieving employment record to edit"
+				});
+
+				return h.redirect(`/cms/employment?${params.toString()}`);
+			}
+		}
+	},
+	{
+		method: "POST",
+		path: '/employment/update',
+		options: {
+			auth: 'customAuth'
+		},
+		handler: async function(req,h) {
+			let params;
+			try {
+				const client = await mongoClient.connect();
+				const db = client.db(DB_NAME);
+
+				let { id, title, employer, description, start_month, start_year } = req['payload'];
+				let submittedData = {
+					title, 
+					employer, 
+					description, 
+					start_month,
+					start_year: parseInt(start_year)
+				}
+
+				if ( 'end_month' in req['payload'] ) {
+					let { end_month, end_year } = req['payload'];
+
+					submittedData = {
+						end_month,
+						end_year: parseInt(end_year),
+						...submittedData
+					}
+				}
+				else {
+					submittedData = {
+						end_month: null,
+						end_year: null
+					}
+				}
+
+				const result = await db.collection(COLLECTION_NAME).findOneAndUpdate({
+					_id: new ObjectId(id)
+				}, {
+					$set: submittedData
+				});
+
+				params = new URLSearchParams({
+					success: `Successfully updated project "${title}"`
+				})
+			} catch(err) {
+				console.log(`Error updating employment record with id: ${id}`);
+				console.log(err);
+
+				params = new URLSearchParams({
+					err: "Error updating employment record"
+				})
+			} finally {
+				return h.redirect(`/cms/employment?${params.toString()}`);
+			}
 		}
 	},
 	{
